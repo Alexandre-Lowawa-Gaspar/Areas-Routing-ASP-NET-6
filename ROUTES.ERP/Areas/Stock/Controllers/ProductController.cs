02b4a46619fde94a3e8973989e37144abac52e8c
Alexandre Lowawa Gaspar <71171768+Alexandre-AGKing@users.noreply.github.com>
2023-07-20 12:15:18 +0100

    Add product class and created the view

---
+++ ROUTES.ERP/Areas/Stock/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ROUTES.ERP.Areas.Stock.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ROUTES.ERP/Areas/Stock/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ROUTES.ERP.Areas.Stock.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
