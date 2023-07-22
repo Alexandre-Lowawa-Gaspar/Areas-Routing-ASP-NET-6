ffd690da6bf2f3816208958c83453ec84d176329
Alexandre Lowawa Gaspar <71171768+Alexandre-AGKing@users.noreply.github.com>
2023-07-22 07:36:19 +0100

    Estou usando a forma tradicional para caminhar entre as páginas, isto é, usando Controllers, Models, Views.
    O modelo de criação do aspnet core 6, criou uma pasta pages, e penso que através dela ele, faz rotas na aplicação, e eu pensei melhor usar o modelo MVC para isso.

--- ROUTES.ERP/Program.cs
+++ ROUTES.ERP/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
 namespace ROUTES.ERP
 {
     public class Program
@@ -7,27 +10,39 @@ namespace ROUTES.ERP
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddRazorPages();
+       
 
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("");
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.MapRazorPages();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Product}/{action=Index}/{id?}"
+                );
 
+                app.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area}/{controller=Home}/{action=Index}/{id?}");
+
+
+                app.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
             app.Run();
         }
     }
